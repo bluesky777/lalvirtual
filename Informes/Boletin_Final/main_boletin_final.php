d31b1838d5a7dc4e36407a7fdad50817aa081e25
Joseth David <davidguerrero777@gmail.com>
2014-11-26 11:07:51 -0500

    Envio al index cuando session expira.

--- Informes/Boletin_Final/main_boletin_final.php
+++ Informes/Boletin_Final/main_boletin_final.php
@@ -1,5 +1,26 @@
 <?php
-require_once("../../verificar_sesion.php");
+session_name("LoginUsuario"); 
+session_start(); //iniciamos la sesión 
+
+//Compruebo que el usuario está logueado 
+if (!isset($_SESSION)){
+	header("location: ../../index.php"); //Nos vamos al menú si ya inicio sesión.
+} else { 
+    //sino, calculamos el tiempo transcurrido 
+    $fechaGuardada = $_SESSION["UltimoAcceso"]; 
+    $ahora = date("Y-n-j H:i:s"); 
+    $tiempo_transcurrido = (strtotime($ahora)-strtotime($fechaGuardada)); 
+	
+	if($tiempo_transcurrido >= 1200) { 
+     //si pasaron 10 minutos (600 seg) o más 
+      session_destroy(); // destruyo la sesión 
+      header("Location: ../../index.php"); //envío al usuario a la pag. de autenticación 
+	}else { 
+	//sino, actualizo la fecha de la sesión 
+	$_SESSION["UltimoAcceso"] = $ahora; 
+	} 
+} 
+
 require_once("../../php/clsConexion.php");
 
 $Con=new clsConexion();
